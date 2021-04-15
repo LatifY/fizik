d45bd179675bfe01ea5bd55c07eee9cac7c60e88
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2021-04-15 12:31:00 +0300

    lottie file changed

--- main.py
+++ main.py
@@ -133,6 +133,8 @@ def noktasal():
             return render_template("pages/optik/noktasal.html", result=result, values=values)
     return render_template("pages/optik/noktasal.html")
 #endregion
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return (render_template('404.html'), 404)
