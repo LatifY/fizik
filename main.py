6f8d872e343023cb03125c3bc9e3ca2d298e0d6f
Latif Yılmaz <latifyilmaza@gmail.com>
2022-06-06 01:54:21 +0300

    za

--- main.py
+++ main.py
@@ -9,51 +9,6 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 def home():
     return render_template('index.html')
 
-#region BASINC
-@app.route("/kati-basinci", methods=["GET","POST"])
-def kati():
-    if request.method == "POST":
-        kuvvet = request.form["kuvvet"]
-        alan = request.form["alan"]
-        if kuvvet and alan:
-            kuvvet = float(kuvvet)
-            alan = float(alan)
-            result = kuvvet/alan
-            values = {"kuvvet":kuvvet,"alan":alan}
-            return render_template("pages/basinc/kati.html", result=result, values=values)
-    return render_template("pages/basinc/kati.html")
-
-@app.route("/sivi-basinci", methods=["GET","POST"])
-def sivi():
-    if request.method == "POST":
-        derinlik = request.form["derinlik"]
-        ozkutle = request.form["ozkutle"]
-        ivme = request.form["ivme"]
-        if derinlik and ozkutle and ivme:
-            derinlik = float(derinlik)
-            ozkutle = float(ozkutle)
-            ivme = float(ivme)
-            result = derinlik * ozkutle * ivme
-            values = {"derinlik":derinlik,"ozkutle":ozkutle, "ivme":ivme}
-            return render_template("pages/basinc/sivi.html", result=result, values=values)
-    return render_template("pages/basinc/sivi.html")
-
-@app.route("/gaz-basinci", methods=["GET","POST"])
-def gaz():
-    if request.method == "POST":
-        hacim = request.form["hacim"]
-        sicaklik = request.form["sicaklik"]
-        tanecik = request.form["tanecik"]
-        if hacim and sicaklik and tanecik:
-            hacim = float(hacim)
-            sicaklik = float(sicaklik)
-            tanecik = float(tanecik)
-            result = sicaklik / hacim * tanecik *8.3145
-            values = {"hacim":hacim,"sicaklik":sicaklik, "tanecik":tanecik}
-            return render_template("pages/basinc/gaz.html", result=result, values=values)
-    return render_template("pages/basinc/gaz.html")
-#endregion
-
 #region ELEKTRIKSEL
 @app.route("/akim", methods=["GET","POST"])
 def akim():
@@ -93,66 +48,94 @@ def gerilim():
             values = {"direnc":direnc,"akim":akim}
             return render_template("pages/elektriksel/gerilim.html", result=result, values=values)
     return render_template("pages/elektriksel/gerilim.html")
-#endregion
 
-#region OPTIK
-@app.route("/isik-akisi", methods=["GET","POST"])
-def aki():
+@app.route("/siga", methods=["GET","POST"])
+def siga():
     if request.method == "POST":
-        siddet = request.form["siddet"]
-        if siddet:
-            siddet = float(siddet)
-            result = 4 * 3.14 * siddet
-            values = {"siddet":siddet}
-            return render_template("pages/optik/aki.html", result=result, values=values)
-    return render_template("pages/optik/aki.html")
+        yuk = request.form["yuk"]
+        potansiyel = request.form["potansiyel"]
+        if yuk and potansiyel:
+            yuk = float(yuk)
+            potansiyel = float(potansiyel)
+            result = yuk / potansiyel
+            values = {"yuk":yuk,"potansiyel":potansiyel}
+            return render_template("pages/elektriksel/siga.html", result=result, values=values)
+    return render_template("pages/elektriksel/siga.html")
 
-@app.route("/ortalama-aydinlanma-siddeti", methods=["GET","POST"])
-def ortalama():
+@app.route("/siga2", methods=["GET","POST"])
+def siga2():
     if request.method == "POST":
-        aki = request.form["aki"]
+        katsayi = request.form["katsayi"]
         alan = request.form["alan"]
-        if aki and alan:
-            aki = float(aki)
-            alan = float(alan)
-            result = aki/alan
-            values = {"aki":aki,"alan":alan}
-            return render_template("pages/optik/ortalama.html", result=result, values=values)
-    return render_template("pages/optik/ortalama.html")
-
-@app.route("/noktasal-aydinlanma-siddeti", methods=["GET","POST"])
-def noktasal():
-    if request.method == "POST":
-        siddet = request.form["siddet"]
         mesafe = request.form["mesafe"]
-        if siddet and mesafe:
-            siddet = float(siddet)
+        if katsayi and alan and mesafe:
+            katsayi = float(katsayi)
+            alan = float(alan)
             mesafe = float(mesafe)
-            result = siddet / (mesafe**2)
-            values = {"siddet":siddet,"mesafe":mesafe}
-            return render_template("pages/optik/noktasal.html", result=result, values=values)
-    return render_template("pages/optik/noktasal.html")
+            result = katsayi * alan / mesafe
+            values = {"katsayi":katsayi,"alan":alan, "mesafe":mesafe}
+            return render_template("pages/elektriksel/siga2.html", result=result, values=values)
+    return render_template("pages/elektriksel/siga2.html")
+
+@app.route("/kuvvet", methods=["GET","POST"])
+def kuvvet():
+    if request.method == "POST":
+        yuk1= request.form["yuk1"]
+        yuk2 = request.form["yuk2"]
+        mesafe = request.form["mesafe"]
+        if yuk1 and yuk2 and mesafe:
+            yuk1 = float(yuk1)
+            yuk2 = float(yuk2)
+            mesafe = float(mesafe)
+            result = yuk1 * yuk2 * 9 / (mesafe * mesafe)
+            values = {"yuk1":yuk1,"yuk2":yuk2, "mesafe":mesafe}
+            return render_template("pages/elektriksel/kuvvet.html", result=result, values=values)
+    return render_template("pages/elektriksel/kuvvet.html")
 #endregion
 
-@app.route("/gezegenlerde-agirlik", methods=["GET","POST"])
-def agirlik():
-    if request.method == "POST":
-        kutle = request.form["kutle"]
-        if kutle:
-            kutle = float(kutle)
-            dunya = round(kutle * 9.8,2)
-            merkur = round(kutle * 3.7,2)
-            mars = round(kutle * 3.72,2)
-            saturn = round(kutle * 10.44,2)
-            uranus = round(kutle * 8.87,2)
-            venus = round(kutle * 8.87,2)
-            neptun = round(kutle * 11.15,2)
-            jupiter = round(kutle * 24.79,2)
-            values = {"kutle":kutle}
-            planets = {"Merkür":merkur, "Venüs":venus, "Dünya":dunya, "Mars":mars, "Jüpiter":jupiter, "Satürn":saturn, "Uranüs":uranus, "Neptün":neptun}
-            return render_template("pages/uzay/agirlik.html", planets=planets, values=values)
-    return render_template("pages/uzay/agirlik.html")
 
+#region ENERJI
+@app.route("/esneklik", methods=["GET","POST"])
+def esneklik():
+    if request.method == "POST":
+        sabit= request.form["sabit"]
+        yol = request.form["yol"]
+        if sabit and yol:
+            sabit = float(sabit)
+            yol = float(yol)
+            result = sabit * yol * yol / 2
+            values = {"sabit":sabit,"yol":yol}
+            return render_template("pages/enerji/esneklik.html", result=result, values=values)
+    return render_template("pages/enerji/esneklik.html")
+
+@app.route("/kinetik", methods=["GET","POST"])
+def kinetik():
+    if request.method == "POST":
+        kutle= request.form["kutle"]
+        hiz = request.form["hiz"]
+        if kutle and hiz:
+            kutle = float(kutle)
+            hiz = float(hiz)
+            result = kutle * hiz * hiz / 2
+            values = {"kutle":kutle,"hiz":hiz}
+            return render_template("pages/enerji/kinetik.html", result=result, values=values)
+    return render_template("pages/enerji/kinetik.html")
+
+@app.route("/potansiyel", methods=["GET","POST"])
+def potansiyel():
+    if request.method == "POST":
+        kutle= request.form["kutle"]
+        yercekimi= request.form["yercekimi"]
+        yukseklik = request.form["yukseklik"]
+        if kutle and yercekimi and yukseklik:
+            kutle = float(kutle)
+            yercekimi = float(yercekimi)
+            yukseklik = float(yukseklik)
+            result = kutle * yercekimi * yukseklik
+            values = {"kutle":kutle,"yercekimi":yercekimi, "yukseklik":yukseklik}
+            return render_template("pages/enerji/potansiyel.html", result=result, values=values)
+    return render_template("pages/enerji/potansiyel.html")
+#endregion
 
 
 @app.errorhandler(404)
